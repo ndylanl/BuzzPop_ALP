309bfd41ead0591fad4444a7d1a1a9ea88515b69
ndylanl <dylanlienardi@gmail.com>
2023-05-09 21:32:52 +0700

    splash

--- BuzzPop_ALP/SplashScreen.swift
+++ BuzzPop_ALP/SplashScreen.swift
@@ -15,19 +15,23 @@ struct SplashScreen_Previews: PreviewProvider {
 
 
 struct SplashScreen: View {
-    @State private var isActive = true
     
     var body: some View {
         VStack {
-            if self.isActive {
-                GameView()
-            } else {
+
                 Image("logo")
                             .resizable()
                             .scaledToFit()
                             .frame(maxWidth: .infinity, maxHeight: .infinity)
                             .background(Color.init(red: 0, green: 85/255, blue: 83/255))
-            }
+                            .onAppear {
+                                DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) { // Change duration time as per your requirement
+                                    withAnimation(.easeOut(duration: 1.0)) {
+                                        
+                                    }
+                                }
+                            }
+            
         }
     }
 }
