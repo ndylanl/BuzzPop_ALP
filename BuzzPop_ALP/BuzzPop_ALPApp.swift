43ff648be2e9227b22665269d48f42c4df11e18f
ndylanl <dylanlienardi@gmail.com>
2023-05-09 18:29:22 +0700

    Initial Commit

---
+++ BuzzPop_ALP/BuzzPop_ALPApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuzzPop_ALPApp.swift
+//  BuzzPop_ALP
+//
+//  Created by MacBook Pro on 09/05/23.
+//
+
+import SwiftUI
+
+@main
+struct BuzzPop_ALPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BuzzPop_ALPUITests/BuzzPop_ALPUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  BuzzPop_ALPUITestsLaunchTests.swift
+//  BuzzPop_ALPUITests
+//
+//  Created by MacBook Pro on 09/05/23.
+//
+
+import XCTest
+
+final class BuzzPop_ALPUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
