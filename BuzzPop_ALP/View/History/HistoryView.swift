ee54f56acb7d4be7a95222e43abb92de4207c555
reefuu <rifqiet@gmail.com>
2023-05-27 06:53:08 +0700

    update graph, not done, need to find the best colors

--- BuzzPop_ALP/View/History/HistoryView.swift
+++ BuzzPop_ALP/View/History/HistoryView.swift
@@ -14,7 +14,7 @@ struct HistoryView: View {
     
     var body: some View {
         let scores = viewModel.score.map {Double($0)}
-        let customdarkstyle = ChartStyle(backgroundColor: Color(red: 20/255, green: 25/255, blue: 35/255, opacity: 0.99), accentColor: Color(red: 232/255, green: 199/255, blue: 137/255), gradientColor: GradientColor(start: .green, end: Color(red: 232/255, green: 199/255, blue: 137/255)), textColor: Color(red: 232/255, green: 199/255, blue: 137/255), legendTextColor: Color(red: 232/255, green: 199/255, blue: 137/255), dropShadowColor: .white)
+        let customdarkstyle = ChartStyle(backgroundColor: Color(red: 20/255, green: 25/255, blue: 35/255, opacity: 0.99), accentColor: Color(red: 232/255, green: 199/255, blue: 137/255), gradientColor: GradientColor(start: Color(red: 232/255, green: 199/255, blue: 137/255), end: .green), textColor: Color(red: 4/255, green: 84/255, blue: 83/255), legendTextColor: Color(red: 232/255, green: 199/255, blue: 137/255), dropShadowColor: .white)
 
         VStack{
             LineView(data: scores, title: "History", legend: "score", style: customdarkstyle, legendSpecifier: "%.0f")
