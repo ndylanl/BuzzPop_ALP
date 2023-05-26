77c517f935d25bcf7b3e9b0a0b71118695eca6a6
reefuu <rifqiet@gmail.com>
2023-05-26 10:33:20 +0700

    update history's chart

--- BuzzPop_ALP/View/History/HistoryView.swift
+++ BuzzPop_ALP/View/History/HistoryView.swift
@@ -14,9 +14,14 @@ struct HistoryView: View {
     
     var body: some View {
         let scores = viewModel.score.map {Double($0)}
+        let customdarkstyle = ChartStyle(backgroundColor: Color(red: 20/255, green: 25/255, blue: 35/255, opacity: 0.99), accentColor: Color(red: 232/255, green: 199/255, blue: 137/255), gradientColor: GradientColor(start: .green, end: Color(red: 232/255, green: 199/255, blue: 137/255)), textColor: Color(red: 232/255, green: 199/255, blue: 137/255), legendTextColor: Color(red: 232/255, green: 199/255, blue: 137/255), dropShadowColor: .white)
 
-        LineView(data: scores, title: "History", legend: "score", legendSpecifier: "%.0f")
-            .padding()
+        VStack{
+            LineView(data: scores, title: "History", legend: "score", style: customdarkstyle, legendSpecifier: "%.0f")
+                .padding()
+            
+        }
+        .background(Color(red: 20/255, green: 25/255, blue: 35/255, opacity: 0.99))
         
         
     }
