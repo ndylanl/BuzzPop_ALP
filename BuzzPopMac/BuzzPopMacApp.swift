3fc729e1026b4c16220b65d3f91fe7709f3f6e96
Hagen-Kwik <92912847+Hagen-Kwik@users.noreply.github.com>
2023-05-26 08:58:32 +0700

    mac
    first view and profile view

---
+++ BuzzPopMac/BuzzPopMacApp.swift
@@ -0,0 +1,18 @@
+//
+//  BuzzPopMacApp.swift
+//  BuzzPopMac
+//
+//  Created by MacBook Pro on 25/05/23.
+//
+
+import SwiftUI
+
+@main
+struct BuzzPopMacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
